9b9bdcaf83fb4bee91fa29349ea549545f056ac2
Taskin Mohammad Mubassir <taskinmubassir@gmail.com>
2026-02-03 14:05:27 +0600

    backend without auth

---
+++ backend/src/configs/database.config.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+  try {
+    // const conn = await mongoose.connect(process.env.MONGODB_URI);
+
+    // const conn = await mongoose.connect(
+    //    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.i90dubc.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+    // );
+
+    const conn = await mongoose.connect(
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ko1jsll.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+    );
+
+    console.log(`✅ DB Connected - Name: ${conn.connection.name}`);
+  } catch (error) {
+    console.error('❎ Database connection error:', error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
---
+++ backend/src/server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+import cors from 'cors';
+import connectDB from './configs/database.config.js';
+import blogRoutes from './routes/blog.route.js';
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+app.get('/', (req, res) => {
+  res.send('SERVER is running...');
+});
+
+app.use('/api/v1/blogs', blogRoutes);
+
+app.listen(PORT, () => {
+  console.log(`✅ Server is running on port ${PORT}`);
+});
